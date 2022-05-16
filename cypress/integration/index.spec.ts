56656e20d5471d248e60ff5f8fa415ab0e622bd6
oahz <pengoahz@gmail.com>
2022-05-16 13:43:58 +0800

    chore: add vitest cypress

---
+++ cypress/integration/index.spec.ts
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+  it('Visits the localhost', () => {
+    cy.visit('http://localhost:3101')
+  })
+})
---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
